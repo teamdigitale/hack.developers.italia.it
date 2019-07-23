9b846d9670efaf90753650a93875e304d77630af
Alessandro Sebastiani <sebbalex@gmail.com>
2019-07-23 12:31:53 +0200

    sedi upgraded to relative paths
    Signed-off-by: Alessandro Sebastiani <sebbalex@gmail.com>

--- replace.sh
+++ replace.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-DIR="/Users/sebbalex/workspace/hack.developers.italia.it/hack.developers.italia.it/$1"
+DIR=$PWD/$1
 
 if [ ! -d "$DIR" ]; then
   echo "$DIR not exists"
@@ -18,7 +18,10 @@ cd $DIR
 for i in $(ls -1)
 do
   echo $i
-  sed -i.bak "s/\?ver=.*'/'/g" $i/index.html
-  sed -i.bak "s/https:\/\/hack.developers.italia.it//g" $i/index.html
-  sed -i.bak  "s/document.location = document.URL.replace(\/\^http:\/i, \"https:\");/\/\/document.location = document.URL.replace(\/\^http:\/i, \"https:\");/g" $i/index.html
+  #sed -i.bak "s/\?ver=.*'/'/g" $i/index.html
+  #sed -i.bak "s/https:\/\/hack.developers.italia.it//g" $i/index.html
+  #sed -i.bak  "s/document.location = document.URL.replace(\/\^http:\/i, \"https:\");/\/\/document.location = document.URL.replace(\/\^http:\/i, \"https:\");/g" $i/index.html
+  sed -i.bak "s/\/wp\-/wp\-/g" $i/index.html
+  sed -i.bak "s/href=\"\//href=\"/g" $i/index.html
+  sed -i.bak "s/jswp\-embed/js\/wp\-embed/g" $i/index.html
 done
